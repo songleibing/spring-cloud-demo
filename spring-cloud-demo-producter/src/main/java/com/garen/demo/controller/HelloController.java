e0d1b53a4ce8320b70b0825de7785c9fac5f9d55
leibing.song <315439102@qq.com>
2019-05-20 14:00:57 +0800

    feign client

--- spring-cloud-demo-producter/src/main/java/com/garen/demo/controller/HelloController.java
+++ spring-cloud-demo-producter/src/main/java/com/garen/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.garen.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("/{name}")
     public String sayHello(@PathVariable("name") String name) {
-        return String.format("Hello %s", name);
+        return String.format("Hello %s from %s", name, port);
     }
 }
